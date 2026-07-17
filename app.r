a245d7fa3bcf40dd41c8e217e447b27b88e5e6a1
Gianna Gatling <giannagatling29@gmail.com>
2026-07-17 14:21:17 -0400

    Refactor Shiny app to use DT for data display

--- app.r
+++ app.r
@@ -1,109 +1,138 @@
 
 
 library(shiny)
-##Upload up to 50 MB
+library(DT)
+## Upload up to 50 MB
 options(shiny.maxRequestSize = 50 * 1024^2)
 
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
   title = "Survey Wave Dashy",
-  tabsetPanel(
-    type = "tabs",
-    tabPanel(
-      "Home",
-      verbatimTextOutput("contents")
-    ),
-    # uploading the big wave merged csv files
-    tabPanel(
-      "Data Upload",
-      sidebarLayout(
-        sidebarPanel(
-          fileInput("file", "Upload Survey ",
-            multiple = FALSE,
-            accept = c(".csv", ".xlsx")
-          ),
-          hr(),
-          selectInput(
-            inputId = "var_sum",
-            label = "Select Variable",
-            choices = NULL
-          )
+
+  # uploading the big wave merged csv files
+  tabPanel(
+    "Data Upload",
+    sidebarLayout(
+      sidebarPanel(
+        fileInput("file", "Upload Survey ",
+          multiple = FALSE,
+          accept = c(".csv", ".xlsx")
         ),
-        mainPanel(
-          h4("Dataset Information"),
-          tableOutput("table"),
-          hr(),
-          verbatimTextOutput("info")
+        hr(),
+        selectizeInput(
+          inputId = "var_sum",
+          label = "Select Variable",
+          choices = NULL
         )
+      ),
+      mainPanel(
+        h4("Dataset Information"),
+        DTOutput("table"),
+        hr(),
+        verbatimTextOutput("info")
       )
-    ),
-    tabPanel(
-      "Survey Summary",
-      verbatimTextOutput("summary")
-    ),
-    tabPanel(
-      "Compare Waves",
-      plotOutput("plot")
-    ),
-    tabPanel(
-      "Statistical Analysis",
-      verbatimTextOutput("static")
-    ),
-    tabPanel(
-      "Dashboard",
-      dataTableOutput("dashboard")
     )
+  ),
+  tabPanel(
+    "Survey Summary",
+    plotOutput("summary")
+  ),
+  tabPanel(
+    "Compare Waves",
+    plotOutput("plot")
+  ),
+  tabPanel(
+    "Statistical Analysis",
+    verbatimTextOutput("static")
+  ),
+  tabPanel(
+    "Dashboard",
+    dataTableOutput("dashboard")
   )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-## Add the reactive element so that you can upload file   
+  ## Add the reactive element so that you can upload file
   df <- reactive({
     req(input$file)
-    read.csv(
+    data<- read.csv(
       input$file$datapath,
-      stringsAsFactors = FALSE)
+      stringsAsFactors = FALSE
+    )
+    expected_col_names <- c("Wave", "MonthYear")
+    given_col_names <- names(data)
+    
+    if(all(expected_col_names %in% given_col_names)) {
+      showModal(
+        modalDialog(
+          title = "Success",
+          p(paste(
+            "Correct File Uploaded. File contains the following columns:", 
+            paste(expected_col_names, collapse = ", ")
+          ))
+        )
+      )
+      
+    } else {
+      showModal(
+        modalDialog(
+          title = "Error",
+          p(paste(
+            "Incorrect File Uploaded. Please make sure your file contains the following columns:", 
+            paste(expected_col_names, collapse = ", ")
+          ))
+        )
+      )
+    }
+    data
     })
   observe({
     req(df())
-    updateSelectInput(
+    updateSelectizeInput(
       session = session,
       inputId = "var_sum",
       choices = names(df())
     )
   })
-## The element to preview the file on the data upload page   
-   output$table<- renderTable({
-     req(df())
-     head(df())
-   }, bordered = TRUE)
-  ##Get a summary of 
-    output$info<- renderPrint({
-      req(df(),input$var_sum)
-      x<-df()[[input$var_sum]]
-      if(is.character(x)){
-        x<- as.factor(x)
-      }
-      if(is.factor(x)){
-        cat("Frequency Table\n\n")
-        print(table(x, useNA = "ifany"))
-      } else if ((is.numeric(x)|| is.integer(x))&& 
-                 length(unique(na.omit(x)))<=10){
-        x<- factor(x)
-        cat("Frequency Table\n\n")
-        print(table(x, useNA = "ifany"))
-      } else if(is.numeric(x)|| is.integer(x)){
-        cat("Numerical Summary\n\n")
-        print(summary(x))
-      } else {
-        cat("Unsupported Variable Type")
-      }
-    })
+  ## The element to preview the file on the data upload page
+  output$table <- renderDT({
+    req(df())
+    datatable(
+      head(df(), 100),
+      options= list(pageLength = 10, scrollX = TRUE),
+      selection = list(target= 'column')
+    )
+  })
+  ## Get a summary of variables 
+  output$info <- renderPrint({
+    req(df(), input$var_sum)
+    x <- df()[[input$var_sum]]
+    if (is.character(x)) {
+      x <- as.factor(x)
+    }
+    if (is.factor(x)) {
+      cat("Frequency Table\n\n")
+      print(table(x, useNA = "ifany"))
+    } else if ((is.numeric(x) || is.integer(x)) &&
+      length(unique(na.omit(x))) <= 10) {
+      x <- factor(x)
+      cat("Frequency Table\n\n")
+      print(table(x, useNA = "ifany"))
+    } else if (is.numeric(x) || is.integer(x)) {
+      cat("Numerical Summary\n\n")
+      print(summary(x))
+    } else {
+      cat("Unsupported Variable Type")
+    }
+  })
   
   
-}
+  
+  
+  }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
